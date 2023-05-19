4edf678fa083aea363221b1f012f46a6f5f53849
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-05-19 11:31:16 +0300

    add db for server

--- server.js
+++ server.js
@@ -3,24 +3,17 @@ require("dotenv").config();
 const app = require("./app");
 
 const mongoose = require("mongoose");
-//******** */
-const dataConvert = require("./helpers/convertNewsDate");
-//******** */
 
 const { PORT = 3030, DB_HOST, MONGO_URI } = process.env;
 // console.log('DB_HOST:\t', DB_HOST);
 // console.log('DB_TEST_HOST:\t', DB_TEST_HOST);
 
 mongoose
-  .connect(
-    "mongodb+srv://Vlad:VladPopoff@cluster0.k5w0ytp.mongodb.net/db-animal-welfare-network?retryWrites=true&w=majority"
-  )
+  .connect(DB_HOST)
   .then(() => {
     app.listen(PORT, () => {
-      // createFolderIsNotExist(TEMP_DIR);
       console.log("Database connection successful");
       console.log(`Server running. Use our API on port: ${PORT}`);
-      // dataConvert(); // convert date from string to Date
     });
   })
   .catch((error) => {
