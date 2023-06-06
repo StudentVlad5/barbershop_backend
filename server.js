48085a419e755ceb0c622f52e1ab32fbf098589e
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-06 14:25:30 +0300

    add connectDB() async

--- server.js
+++ server.js
@@ -4,12 +4,21 @@ const app = require("./app");
 
 const mongoose = require("mongoose");
 
-const { PORT = 3030, DB_HOST, MONGO_URI } = process.env;
+const { PORT = 3030, DB_HOST } = process.env;
 // console.log('DB_HOST:\t', DB_HOST);
 // console.log('DB_TEST_HOST:\t', DB_TEST_HOST);
 
-mongoose
-  .connect(DB_HOST)
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_HOST);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+connectDB()
   .then(() => {
     app.listen(PORT, () => {
       console.log("Database connection successful");
