2957591207b903055a93cc67314b0e9dabfa525f
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-09 14:01:34 +0300

    Messages.create

--- controllers/message/post.js
+++ controllers/message/post.js
@@ -4,7 +4,7 @@ const post = async (req, res, next) => {
   const { name, phone, message } = req.body;
   console.log(req.body);
   try {
-    // await Messages.create({ name, phone, message });
+    await Messages.create({ name, phone, message });
     return res.status(200).json({ message: "Success status" });
   } catch (error) {
     return res.status(400).json({ message: "Invalid search characters" });
