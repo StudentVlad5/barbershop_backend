0a759e1dcdb3d70dd3a34abec6adfbc4cf82d73b
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-13 10:16:35 +0300

    admin + services

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -1,52 +1,52 @@
-const express = require('express');
-// const { user, services } = require('../../controllers');
-const { user } = require('../../controllers');
+const express = require("express");
+const { user, services } = require("../../controllers");
+
 const {
   ctrlWrapper,
   authMiddleware,
   validation,
   uploadCloud,
-} = require('../../middleWares');
-const { userUpdateValidationSchema } = require('../../models');
+} = require("../../middleWares");
+const { userUpdateValidationSchema } = require("../../models");
 
 const router = express.Router();
 
-router.get('/users', ctrlWrapper(authMiddleware), ctrlWrapper(user.getUsers));
+router.get("/users", ctrlWrapper(authMiddleware), ctrlWrapper(user.getUsers));
 router.get(
-  '/users/:id',
+  "/users/:id",
   ctrlWrapper(authMiddleware),
   ctrlWrapper(user.getUserById)
 );
 router.delete(
-  '/users/:id',
+  "/users/:id",
   ctrlWrapper(authMiddleware),
   ctrlWrapper(user.deleteUsers)
 );
 
 router.patch(
-  '/users/:id',
+  "/users/:id",
   ctrlWrapper(authMiddleware),
-  uploadCloud.single('avatar'),
+  uploadCloud.single("avatar"),
   validation(userUpdateValidationSchema),
   ctrlWrapper(user.updateUser)
 );
 
-// router.get(
-//   '/services',
-//   ctrlWrapper(authMiddleware),
-//   ctrlWrapper(services.getServices)
-// );
+router.get(
+  "/services",
+  ctrlWrapper(authMiddleware),
+  ctrlWrapper(services.getServices)
+);
 
-// router.delete(
-//   '/services/:id',
-//   ctrlWrapper(authMiddleware),
-//   ctrlWrapper(services.deleteServices)
-// );
+router.delete(
+  "/services/:id",
+  ctrlWrapper(authMiddleware),
+  ctrlWrapper(services.deleteServices)
+);
 
-// router.patch(
-//   '/services/:id',
-//   ctrlWrapper(authMiddleware),
-//   ctrlWrapper(services.updateService)
-// );
+router.patch(
+  "/services/:id",
+  ctrlWrapper(authMiddleware),
+  ctrlWrapper(services.updateService)
+);
 
 module.exports = routerAdmin = router;
