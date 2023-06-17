fb43387886c5352d07369e155808293146a891e9
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-17 13:16:00 +0300

    update/auth

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,15 +1,24 @@
+const { ValidationError } = require("../../helpers");
+const { Users } = require("../../models");
+const {
+  userMainField,
+  userFieldReceivedFromFront,
+  dataFilter,
+} = require("../../helpers");
+
 const update = async (req, res, next) => {
-     try {
-      const newData = dataFilter(req.body, userFieldReceivedFromFront);
-      req.file?.path && (newData.avatar = req.file.path);
-      const resUpdate = await Users.findByIdAndUpdate({ _id: id }, newData, {
-        new: true,
-      });
-      const newResponse = dataFilter(resUpdate, userMainField);
-      req.file?.path && (newResponse.avatar = req.file.path);
-      return res.status(201).json(newResponse);
-    } catch (err) {
-      throw new ValidationError(err.message);
-    }
-  };
+  const { id } = req.params;
+  try {
+    const newData = dataFilter(req.body, userFieldReceivedFromFront);
+    req.file?.path && (newData.avatar = req.file.path);
+    const resUpdate = await Users.findByIdAndUpdate({ _id: id }, newData, {
+      new: true,
+    });
+    const newResponse = dataFilter(resUpdate, userMainField);
+    req.file?.path && (newResponse.avatar = req.file.path);
+    return res.status(201).json(newResponse);
+  } catch (err) {
+    throw new ValidationError(err.message);
+  }
+};
 module.exports = update;
