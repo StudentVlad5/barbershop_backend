6610d0a211ccd525873d7197878ab02aa83ce72c
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-17 12:05:08 +0300

    add update auth

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -1,3 +1,15 @@
-const update = async (req, res, next) => {};
-
+const update = async (req, res, next) => {
+     try {
+      const newData = dataFilter(req.body, userFieldReceivedFromFront);
+      req.file?.path && (newData.avatar = req.file.path);
+      const resUpdate = await Users.findByIdAndUpdate({ _id: id }, newData, {
+        new: true,
+      });
+      const newResponse = dataFilter(resUpdate, userMainField);
+      req.file?.path && (newResponse.avatar = req.file.path);
+      return res.status(201).json(newResponse);
+    } catch (err) {
+      throw new ValidationError(err.message);
+    }
+  };
 module.exports = update;
