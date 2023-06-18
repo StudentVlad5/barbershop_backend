45b4171cf5db3b95443a329c54b81919915cb524
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-18 18:58:25 +0300

    one more

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -8,17 +8,22 @@ const {
 
 const update = async (req, res, next) => {
   const { id } = req.params;
-  try {
+    console.log("newData", newData);
     const newData = dataFilter(req.body, userFieldReceivedFromFront);
+    if (!newData) {
+      throw new ValidationError("Bad request, invalid data");
+    }
+    console.log("newData", newData);
     req.file?.path && (newData.avatar = req.file.path);
     const resUpdate = await Users.findByIdAndUpdate({ _id: id }, newData, {
       new: true,
     });
-    const newResponse = dataFilter(resUpdate, userMainField);
+    console.log("resUpdate", resUpdate);
+    // const newResponse = dataFilter(resUpdate, userMainField);
     // req.file?.path && (newResponse.avatar = req.file.path);
-    return res.status(201).json(newResponse);
-  } catch (err) {
-    throw new ValidationError(err.message);
-  }
+    console.log("newResponse", newResponse);
+    res
+    .status(201)
+    .json({ code: "201", message: "profile update", data: newResponse });
 };
 module.exports = update;
