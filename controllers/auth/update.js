dd041e287dd771ca03172eee68630359d38e4192
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-18 19:55:56 +0300

    fix path

--- controllers/auth/update.js
+++ controllers/auth/update.js
@@ -8,20 +8,16 @@ const {
 
 const update = async (req, res, next) => {
   const { id } = req.params;
-  console.log("id", id);
      const newData = dataFilter(req.body, userFieldReceivedFromFront);
     if (!newData) {
       throw new ValidationError("Bad request, invalid data");
     }
-    console.log("newData", newData);
     req.file?.path && (newData.avatar = req.file.path);
     const resUpdate = await Users.findByIdAndUpdate({ _id: id }, newData, {
       new: true,
     });
-    console.log("resUpdate", resUpdate);
-    // const newResponse = dataFilter(resUpdate, userMainField);
-    // req.file?.path && (newResponse.avatar = req.file.path);
-    console.log("newResponse", newResponse);
+    const newResponse = dataFilter(resUpdate, userMainField);
+    req.file?.path && (newResponse.avatar = req.file.path);
     res
     .status(201)
     .json({ code: "201", message: "profile update", data: newResponse });
