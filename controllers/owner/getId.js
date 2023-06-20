9c46323d7bf8f1d9b50221ee67d955a76650f2c9
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-20 12:42:00 +0300

    console

--- controllers/owner/getId.js
+++ controllers/owner/getId.js
@@ -4,6 +4,7 @@ const getOwnerById = async (req, res, next) => {
   const id = req.params.id;
   try {
     const owner = await Owner.findById({ _id: id });
+    console.log(owner)
     res.status(200).json(owner);
   } catch (error) {
     throw new WrongIdError(error.message);
