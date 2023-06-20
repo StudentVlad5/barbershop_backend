36b64799d23873b8bbee2bc76105f25aa9858955
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-20 08:10:16 +0300

    get owner byId

--- routes/api/owner.js
+++ routes/api/owner.js
@@ -2,12 +2,11 @@ const express = require("express");
 const { owner } = require("../../controllers");
 const { ctrlWrapper } = require("../../middleWares");
 
-const {
-  get,
-} = owner;
+const { get, getOwnerById } = owner;
 
 const router = express.Router();
 
 router.get("/", ctrlWrapper(get));
+router.get("/:id/", ctrlWrapper(getOwnerById));
 
 module.exports = routerOwner = router;
