84253328ea7c7913a7cac6ce13149a3c89a5e6b6
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-22 10:03:14 +0300

    remove console.log(user);

--- controllers/user/getUserEvents.js
+++ controllers/user/getUserEvents.js
@@ -4,7 +4,6 @@ const getUserEvents = async (req, res, next) => {
   const id = req.params.id;
   try {
     const user = await Event.find({}, { CreateId: id });
-    console.log(user);
     // const newResponse = dataFilter(user, userMainField);
     res.status(200).json(user);
   } catch (error) {
