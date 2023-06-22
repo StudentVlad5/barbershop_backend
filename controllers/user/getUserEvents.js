05ac4e6b3b66c034c50e64ab340d3bc73239f324
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-22 09:50:37 +0300

    cLog

--- controllers/user/getUserEvents.js
+++ controllers/user/getUserEvents.js
@@ -3,7 +3,9 @@ const { Event } = require("../../models");
 const getUserEvents = async (req, res, next) => {
   const id = req.params.id;
   try {
+    console.log(id);
     const user = await Event.findById({ CreateId: id });
+    console.log(user);
     // const newResponse = dataFilter(user, userMainField);
     res.status(200).json(user);
   } catch (error) {
