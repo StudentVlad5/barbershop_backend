75166b923946aac714cb8ab843057f7486abffab
StudentVlad5 <99014125+StudentVlad5@users.noreply.github.com>
2023-06-22 10:14:35 +0300

    Event.find

--- controllers/user/getUserEvents.js
+++ controllers/user/getUserEvents.js
@@ -3,7 +3,10 @@ const { Event } = require("../../models");
 const getUserEvents = async (req, res, next) => {
   const id = req.params.id;
   try {
-    const user = await Event.find({}, { CreateId: id });
+    const user = await Event.find(
+      {},
+      { CreateId: id, StartTime: 1, Subject: 1, OwnerId: 1 }
+    );
     // const newResponse = dataFilter(user, userMainField);
     res.status(200).json(user);
   } catch (error) {
